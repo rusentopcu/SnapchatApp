900973e5c36bd4c2249c31401d13d8d711b6d060
Rusen Topcu <rusentopcu@msn.com>
2020-11-15 13:40:57 +0300

    project created

---
+++ SnapchatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatApp
+//
+//  Created by Rusen Topcu on 15.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
