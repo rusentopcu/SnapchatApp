27a2a10bf7513eb3e5622d5293a253cc57c8c4df
Rusen Topcu <rusentopcu@msn.com>
2020-11-17 12:47:53 +0300

    Select Image

--- SnapchatApp/ViewControllers/UploadVC.swift
+++ SnapchatApp/ViewControllers/UploadVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class UploadVC: UIViewController {
+class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
     
     @IBOutlet weak var uploadImageView: UIImageView!
@@ -15,7 +15,23 @@ class UploadVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        uploadImageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        uploadImageView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func chooseImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        uploadImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
 
