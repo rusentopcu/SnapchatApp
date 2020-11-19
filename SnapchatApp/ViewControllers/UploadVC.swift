af17262870d990353f78ea105e6edc66cff2ad92
Rusen Topcu <rusentopcu@msn.com>
2020-11-19 15:13:59 +0300

    Image Upload in firebase

--- SnapchatApp/ViewControllers/UploadVC.swift
+++ SnapchatApp/ViewControllers/UploadVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
@@ -36,6 +37,57 @@ class UploadVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     
 
     @IBAction func uploadClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        
+        let mediaReference = storageReference.child("media")
+        
+        if let imagedata = uploadImageView.image?.jpegData(compressionQuality: 0.5) {
+            
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaReference.child("\(uuid).jpg")
+            
+            imageReference.putData(imagedata, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "ERROR")
+                }
+                else {
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            
+                            let imageUrl = url?.absoluteString
+                            
+                            let firestore = Firestore.firestore()
+                            
+                            let snapDictionary = ["imageUrl": imageUrl!, "snapOwner": UserSingleton.sharedUserInfo.username,"date": FieldValue.serverTimestamp()] as [String: Any]
+                            
+                            firestore.collection("Snaps").addDocument(data: snapDictionary) { (error) in
+                                if error != nil {
+                                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "ERRor")
+                                }
+                                else {
+                                    self.tabBarController?.selectedIndex = 0
+                                    self.uploadImageView.image = UIImage(named: "select")
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    @objc func makeAlert(title: String, message: String) {
+        
+        let alarm = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alarm.addAction(okButton)
+        self.present(alarm, animated: true, completion: nil)
     }
     
 
